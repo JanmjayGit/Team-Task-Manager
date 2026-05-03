ad6db5cb30d3ccf55914b1584b67e0d3c05cecec
Janmjay <codewithjanmjay@gmail.com>
2026-05-03 11:07:21 +0530

    backend url added and vercel.json added

--- Frontend/src/api/apiEndpoints.js
+++ Frontend/src/api/apiEndpoints.js
@@ -1,5 +1,5 @@
-// const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:8080';
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+// const BASE_URL = 'http://localhost:8080'
 
 export const apiEndpoints = {
   LOGIN: `${BASE_URL}/api/auth/login`,
